b6e8f2a4c950d96fe6c4dc3304bb0b9c22027d56
Domi <vataidomonkos@gmail.com>
2022-11-06 13:20:07 +0100

    lights adding/removing and light placement tracking added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {
   playButton,
   createButton,
   saveButton,
-  gameField,
+  gameFieldContainer,
 } from './references.js';
 
 //States:
@@ -14,10 +14,33 @@ let success = false;
 
 //Events:
 playButton.addEventListener('click', () => onPlayButtonClick());
-
 const onPlayButtonClick = () => {
-  gameField.innerHTML = renderMap(selectedMap);
-  console.log(selectedMap);
+  gameFieldContainer.innerHTML = renderMap(selectedMap);
+};
+
+const getIndexes = (e) => {};
+
+gameFieldContainer.addEventListener('click', (e) => onGameCellClick(e));
+const onGameCellClick = (e) => {
+  if (!e.target.matches('.white')) {
+    console.log('bad target');
+    return;
+  } else {
+    if (e.target.innerHTML == lightbulb) {
+      e.target.innerHTML = '';
+      const indexOfObject = palcedLights.findIndex((element) => {
+        return (
+          element[0] === e.target.parentElement.rowIndex &&
+          element[1] === e.target.cellIndex
+        );
+      });
+      palcedLights.splice(indexOfObject, 1);
+    } else {
+      e.target.innerHTML = lightbulb;
+      const newLamp = [e.target.parentElement.rowIndex, e.target.cellIndex];
+      palcedLights.push(newLamp);
+    }
+  }
 };
 
 //Templates:
@@ -32,7 +55,7 @@ const renderRow = (gameRow) => {
 };
 
 const renderCell = (gameCell) => {
-  return `<td class="gameCell" style="background-color:${
+  return `<td class="gameCell ${gameCell.type}" style="background-color:${
     gameCell.type
   }">${renderNumber(gameCell)}</td>`;
 };
--- references.js
+++ references.js
@@ -1,4 +1,6 @@
 export const playButton = document.querySelector('#play_button');
 export const createButton = document.querySelector('#create_button');
 export const saveButton = document.querySelector('#save_button');
-export const gameField = document.querySelector('#game_field_container');
+export const gameFieldContainer = document.querySelector(
+  '#game_field_container'
+);
