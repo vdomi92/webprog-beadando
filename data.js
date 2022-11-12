41f47a5d1fc027df8edcc6a21fbcaeff2ef47a2c
Domi <vataidomonkos@gmail.com>
2022-11-12 19:49:48 +0100

    light movement and lightbulb placemen

--- data.js
+++ data.js
@@ -1,31 +1,47 @@
-const w0 = {
+export const w0 = {
   level: 0,
   type: 'white',
 };
-const b0 = {
+export const b0 = {
   level: 0,
   type: 'black',
 };
-const b1 = {
+export const b1 = {
   level: 1,
   type: 'black',
 };
-const b2 = {
+export const b2 = {
   level: 2,
   type: 'black',
 };
-const b3 = {
+export const b3 = {
   level: 3,
   type: 'black',
 };
-const b4 = {
+export const b4 = {
   level: 4,
   type: 'black',
 };
-const b5 = {
+export const b5 = {
   level: '',
   type: 'black',
 };
+export const l1 = {
+  level: 1,
+  type: 'yellow'
+}
+export const l2 = {
+  level: 2,
+  type: 'yellow'
+}
+export const l3 = {
+  level: 3,
+  type: 'yellow'
+}
+export const lb = {
+  level: 1,
+  type: 'lightBulb'
+}
 export const map1 = {
     name: 'easy_map1',
     map: [
@@ -37,4 +53,6 @@ export const map1 = {
           [w0, b5, w0, w0, w0, b2, w0],
           [w0, w0, w0, b3, w0, w0, w0],
     ],
+    rows: 7,
+    cols: 7,
   };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { map1 } from './data.js';
+import { map1, w0, b0, b1, b2, b3, b4, b5, l1, l2, l3, lb } from './data.js';
 import {
   playButton,
   createButton,
@@ -7,42 +7,115 @@ import {
 } from './references.js';
 
 //States:
-let selectedMap = map1.map;
-let palcedLights = [];
+let selectedMap = map1;
+let currentMap = '';
 let error = false;
 let success = false;
 
 //Events:
 playButton.addEventListener('click', () => onPlayButtonClick());
 const onPlayButtonClick = () => {
-  gameFieldContainer.innerHTML = renderMap(selectedMap);
+  gameFieldContainer.innerHTML = renderMap(selectedMap.map);
+  currentMap = selectedMap.map;
 };
-
-const getIndexes = (e) => {};
 
 gameFieldContainer.addEventListener('click', (e) => onGameCellClick(e));
 const onGameCellClick = (e) => {
-  if (!e.target.matches('.white')) {
-    console.log('bad target');
-    return;
-  } else {
-    if (e.target.innerHTML == lightbulb) {
-      e.target.innerHTML = '';
-      const indexOfObject = palcedLights.findIndex((element) => {
-        return (
-          element[0] === e.target.parentElement.rowIndex &&
-          element[1] === e.target.cellIndex
-        );
-      });
-      palcedLights.splice(indexOfObject, 1);
-    } else {
-      e.target.innerHTML = lightbulb;
-      const newLamp = [e.target.parentElement.rowIndex, e.target.cellIndex];
-      palcedLights.push(newLamp);
-    }
+  let rowIndex = e.target.parentElement.rowIndex;
+  let cellIndex = e.target.cellIndex;
+  let target = currentMap[rowIndex][cellIndex];
+  lightBulbAction(target, rowIndex, cellIndex);
+};
+//Utilities
+const lightBulbAction = (target, rowIndex, cellIndex) => {
+  if (target == w0) {
+    addLightBulb(rowIndex, cellIndex);
   }
 };
 
+const addLightBulb = (rowIndex, cellIndex) => {
+  currentMap[rowIndex][cellIndex] = lb;
+  gameFieldContainer.innerHTML = renderMap(currentMap);
+  console.log(rowIndex, cellIndex);
+  setTimeout(() => {
+    letThereBeLight(rowIndex, cellIndex);
+  }, '500');
+};
+
+const letThereBeLight = (rowIndex, cellIndex) => {
+  if (rowIndex > 0) lightUp(rowIndex - 1, cellIndex);
+  if (cellIndex > 0) lightLeft(rowIndex, cellIndex - 1);
+  if (cellIndex < selectedMap.cols - 1) lightRight(rowIndex, cellIndex + 1);
+  if (rowIndex < selectedMap.rows - 1) lightDown(rowIndex + 1, cellIndex);
+};
+
+const lightUp = (rowIndex, cellIndex) => {
+  if (currentMap[rowIndex][cellIndex].type === 'black') {
+    console.log('block reached');
+    return;
+  }
+  changeLightFields(rowIndex, cellIndex);
+  gameFieldContainer.innerHTML = renderMap(currentMap);
+  if (rowIndex > 0) {
+    setTimeout(() => {
+      lightUp(rowIndex - 1, cellIndex);
+    }, '500');
+  }
+};
+const lightLeft = (rowIndex, cellIndex) => {
+  if (currentMap[rowIndex][cellIndex].type === 'black') {
+    console.log('block reached');
+    return;
+  }
+  changeLightFields(rowIndex, cellIndex);
+  gameFieldContainer.innerHTML = renderMap(currentMap);
+  if (cellIndex > 0) {
+    setTimeout(() => {
+      lightLeft(rowIndex, cellIndex - 1);
+    }, '500');
+  }
+};
+const lightRight = (rowIndex, cellIndex) => {
+  if (currentMap[rowIndex][cellIndex].type === 'black') {
+    console.log('block reached');
+    return;
+  }
+  changeLightFields(rowIndex, cellIndex);
+  gameFieldContainer.innerHTML = renderMap(currentMap);
+  if (cellIndex < selectedMap.cols - 1) {
+    setTimeout(() => {
+      lightRight(rowIndex, cellIndex + 1);
+    }, '500');
+  }
+};
+const lightDown = (rowIndex, cellIndex) => {
+  if (currentMap[rowIndex][cellIndex].type === 'black') {
+    console.log('block reached');
+    return;
+  }
+  changeLightFields(rowIndex, cellIndex);
+  gameFieldContainer.innerHTML = renderMap(currentMap);
+  if (rowIndex < selectedMap.rows - 1) {
+    setTimeout(() => {
+      lightDown(rowIndex + 1, cellIndex);
+    }, '500');
+  }
+};
+
+const changeLightFields = (rowIndex, cellIndex) => {
+  if (currentMap[rowIndex][cellIndex] == lb) {
+    error = true;
+    console.log('lightbulb hit');
+  }
+  if (currentMap[rowIndex][cellIndex] == w0) {
+    currentMap[rowIndex][cellIndex] = l1;
+  } else if (currentMap[rowIndex][cellIndex] == l1) {
+    currentMap[rowIndex][cellIndex] = l2;
+  } else if (currentMap[rowIndex][cellIndex] == l2) {
+    currentMap[rowIndex][cellIndex] = l3;
+    error = true;
+  }
+};
 //Templates:
 const renderMap = (selectedMap) => {
   return `<table id="game_field">${selectedMap
@@ -55,16 +128,29 @@ const renderRow = (gameRow) => {
 };
 
 const renderCell = (gameCell) => {
-  return `<td class="gameCell ${gameCell.type}" style="background-color:${
-    gameCell.type
-  }">${renderNumber(gameCell)}</td>`;
+  if (gameCell.type === 'lightBulb') {
+    return `<td class="gameCell ${gameCell.type}">${renderNumber(
+      gameCell
+    )}</td>`;
+  } else {
+    return `<td class="gameCell ${gameCell.type}" style="background-color:${
+      gameCell.type
+    }">${renderNumber(gameCell)}</td>`;
+  }
 };
 
 const renderNumber = (gameCell) => {
-  if (gameCell.type == 'white') {
+  if (gameCell.type === 'white') {
     return '';
-  } else if (gameCell.type == 'black') {
+  }
+  if (gameCell.type === 'black') {
     return `${gameCell.level}`;
+  }
+  if (gameCell.type === 'yellow') {
+    return '';
+  }
+  if (gameCell.type === 'lightBulb') {
+    return `${lightbulb}`;
   }
 };
 
